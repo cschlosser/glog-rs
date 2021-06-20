cd51a5e1f20de9fe5f78df89a9d2a2573de4ac7a
Christoph Schlosser <christoph@linux.com>
2021-06-20 02:16:15 +0200

    Split example file

--- examples/main.rs
+++ examples/main.rs
@@ -1,40 +1,24 @@
 use log::*;
 use glog::Flags;
 
-fn foo() {
-    use std::thread;
-
-let handler = thread::Builder::new()
-    .name("named thread".into())
-    .spawn(|| {
-        let handle = thread::current();
-        info!("from other thread?");
-        debug!("a debug msg");
-        trace!("some tracing");
-        assert_eq!(handle.name(), Some("named thread"));
-    })
-    .unwrap();
-
-handler.join().unwrap();
-
-    warn!("a warning from foo()");
-}
+mod foo;
 
 fn main() {
     glog::new()
         .with_year(true)
         .limited_abbreviations(true)
+        .set_application_fingerprint("Example")
         .init(Flags {
             colorlogtostderr: true,
             minloglevel: Level::Trace,
-            log_backtrace_at: Some("main.rs:11".to_owned()),
-            logtostderr: true,
+            log_backtrace_at: Some("main.rs:20".to_owned()),
+            alsologtostderr: true,
             ..Default::default()
         })
         .unwrap();
 
-    error!("some failure in main while testing the logger");
+    error!("some erro in main while testing the logger");
 
-    foo();    
+    foo::foo();    
 }
 
