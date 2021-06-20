5db378bcd15a661ae2b2a7c0c0ed76896712d5d4
Christoph Schlosser <2466365+cschlosser@users.noreply.github.com>
2021-06-20 16:27:01 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() {
     #[cfg(target_os = "windows")]
     windows::build! {
-        Windows::Win32::System::Threading::THREADINFOCLASS,
-        Windows::Win32::System::Threading::THREAD_INFORMATION_CLASS
+        Windows::Win32::System::Threading::GetCurrentThreadId
     };
 }
